9dd03f78aa41524d10363a1d6e56dde3d281c454
z851451199 <851451199@qq.com>
2020-08-04 23:55:40 +0800

    js差brand家居详情选择

--- Demo1/js/brand.js
+++ Demo1/js/brand.js
@@ -10,15 +10,62 @@ $(function(){
 	        var hot_chose = $('.hot_chose')
 	        var str = "";
 	        $.each(data.hotBrand,function (i,item){
-	            str += "<span name="+item.id+">"+item.name+"</span>";
+	            str += "<span>"+item.name+"</span>";
 	        })
-	        hot_chose.append(str)
+            hot_chose.append(str);
+            // 展示已选择品牌（链接传回来的）
+            showBrandList();
 	    }
-	});
-    var hotChose_span = $('.hot_chose span'),
-        show_chose = $('.show_chose');
-	// 创建数组 存放已选标签
-    var brandList = [];
+    });
+    // 创建数组 存放已选标签
+    var show_chose = $('.show_chose');
+    var brandList = [],arrylist1='',arrylist2='';
+    // 展示已选择品牌（链接传回来的） 封装函数 ajax中引入
+    function showBrandList(){
+        var str = ''
+        if(brandList.length == 0){
+            // 无选择品牌时，展示字体
+            $('.null_chose').css({
+                display:'block'
+            })
+        }else{
+            // 有选择品牌时，隐藏字体
+            $('.null_chose').css({
+                display:'none'
+            })
+            // 将已选择品牌加载到页面
+            for(let i of brandList){
+                str += '<p class="sChose_p"><span class="sCp_sp">'+i+'</span><span>x</span></p>';
+            }
+        }
+        show_chose.append(str)
+    }
+    /*
+    取链接url参数
+    */  
+   function getUrl(name){
+        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)","i");
+        var r = window.location.search.substr(1).match(reg);
+        if(r!=null){
+            return decodeURI(r[2]);
+        }else{
+            return null;
+        }
+    }
+    function parameter(){
+        arrylist1 = getUrl("arrylist1");
+        arrylist2 = getUrl("arrylist2");
+        NewList = getUrl("brandList");
+        if(NewList){
+            brandList = NewList.split(',')
+            console.log('susses')
+        }
+    }
+    parameter()
+    /*------*/ 
+
+    
+    
 	// 热门品牌 点击事件
 	$(document).on("click",'.hot_chose span', function(){
 		var showChose_child = $('.show_chose p')
@@ -29,8 +76,6 @@ $(function(){
 		    alert('品牌订阅已达上限')
 		}else{
 		    brandList.push($(this).text())
-			// brandID.push($(this).attr(name))
-			console.log($(this).attr(name))
 		    brandList= [...new Set(brandList)];
 		    var str = ''
 		    for(let i of brandList){
@@ -41,7 +86,7 @@ $(function(){
 		}
 		changeUrl()
 	})
-	// 我的品牌展示  删除品牌
+	// 我的品牌展示  删除品牌  点击事件
     $(document).on("click",'.sChose_p', function(){
         var sCp_sptext = brandList.indexOf($(this).children('.sCp_sp').text())
         brandList.splice(sCp_sptext,1)
@@ -55,12 +100,7 @@ $(function(){
     });
     /* 当选中标签后时  将选中内容通过url参传数  */ 
     function changeUrl(){
-        // if(brandList.length>0){
-        //     $('.hot_Jump').html('<a href="industry.html?brand='+brandList+'" class="active">我没公司，去选行业<span>></span></a>')
-        // }else{
-        //     $('.hot_Jump').html('<a href="industry.html" class="active">我没公司，去选行业<span>></span></a>')
-        // }
-        $('.hot_Jump').html('<a href="industry.html?brand='+brandList+'" class="active">我没公司，去选行业<span>></span></a>')
+        $('.hot_Jump a').attr('href','industry.html?brand='+brandList+'&arrylist1='+arrylist1+'&arrylist2='+arrylist2+'')
     }
 	
 })
--- Demo1/js/industry.js
+++ Demo1/js/industry.js
@@ -2,6 +2,9 @@ $(function(){
     /*
     取链接url参数
     */  
+   var arrylist1 = [],
+       arrylist2 = [],
+       brandList = [];
     function getUrl(name){
         var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)","i");
         var r = window.location.search.substr(1).match(reg);
@@ -12,9 +15,14 @@ $(function(){
         }
     }
 	function addBanner(){
-		var name = getUrl("brand");
-		if(name){
-            var brandList = name.split(',')
+        var brandArry = getUrl("brand");
+        var Newlist1 = getUrl("arrylist1");
+        var Newlist2 = getUrl("arrylist2");
+        arrylist1 = None(Newlist1)
+        arrylist2 = None(Newlist2)
+        // addClass(arrylist1,'.list2_chose span')
+		if(brandArry){
+            brandList = brandArry.split(',')
 			var str = ''
 			for(let i of brandList){
 			    str += '<span class="sCp_sp">'+i+'</span>';
@@ -27,19 +35,117 @@ $(function(){
 			$('.bs_noChose').css({
 				display:'block'
 			})
-		}
-		
-	}
+        }
+        // console.log(arrylist1)
+        // console.log(arrylist2)
+    }
+    function None(name){
+        if(name){
+            return name = name.split(",");
+        }else{
+            return []
+        }
+    }
+    /* 封装函数  对数组已存内容 添加样式 */ 
+    function addClass(name,className){
+        var span = $(''+className+'')
+        var spanText = span.map(function(){
+            return $(this).text();
+          }).toArray();
+        for(let i of name){
+            for(let j of spanText){          
+                if(i == j){
+                    console.log(spanText.indexOf(j))
+                    span.eq(spanText.indexOf(j)).addClass('active')
+                }
+            }
+        }
+
+    }
     addBanner()
+
+    // ajax 获取数据
+	$.ajax({
+	    url:"./data/main.json",
+	    type:"get",
+	    dataType:"json",
+	    async:true,
+	    data:"",
+	    success:function(data){
+            var list1_chose = $('.list1_chose'),
+                list2_chose = $('.list2_chose'),
+                list3_chose = $('.list3_chose');
+            var str = "",
+                str2 = "",
+                str3 = "";
+	        $.each(data.list_chose,function (i,item){
+                if(i == 0){
+                    str += "<span class='showlist2'>"+item.name+"</span>";
+                }else if(i>0 && i<=2){
+                    str += "<span>"+item.name+"</span>";
+                }else{
+                    str3 += "<span>"+item.name+"</span>";
+                }
+            })
+            $.each(data.list_detail,function (i,item){
+                str2 += "<span>"+item.name+"</span>";
+	        })
+            list1_chose.append(str);
+            list2_chose.append(str2);
+            list3_chose.append(str3);
+            // 
+            changeUrl()
+            /*  将返回已选数据  进行重新添加样式  */ 
+            addClass(arrylist1,'.list_chose span')
+            addClass(arrylist2,'.list2_chose span')
+
+	    }
+    });
+    /* ------ */ 
+
     /* 事件 */ 
-    var oShowl2 = $('.showlist2'),
-    list2_chose = $('.list2_chose'),
-    list_span = $('.list_chose span:gt(0)');
-    oShowl2.on('click',function(){
+    $(document).on("click",'.showlist2', function(){
         $('.list2_chose').toggle()
     });
-    list_span.on('click',function(){
+    $(document).on("click",'.list_chose span:gt(0)', function(){
+        // 重数组中添加品牌名称
+        text = $(this).text()
+        arrylist1.push($(this).text())
+        arrylist1= [...new Set(arrylist1)];
+        console.log(arrylist1)
+        // 给选中标签添加类名
         $(this).toggleClass('active')
+        changeUrl()
     });
+    $(document).on("click",'.list_chose .active', function(){
+        // 重数组中移出品牌名称
+        text = $(this).text()
+        textIndex = arrylist1.indexOf(text)
+        arrylist1.splice(textIndex,1)
+        console.log(arrylist1)
+        changeUrl()
+    })
+    $(document).on("click",'.list2_chose span', function(){
+        // 重数组中添加品牌名称
+        text = $(this).text()
+        arrylist2.push($(this).text())
+        arrylist2= [...new Set(arrylist2)];
+        console.log(arrylist2)
+        // 给选中标签添加类名
+        $(this).toggleClass('active')
+        changeUrl()
+    });
+    $(document).on("click",'.list2_chose .active', function(){
+        // 重数组中移出品牌名称
+        text = $(this).text()
+        textIndex = arrylist2.indexOf(text)
+        arrylist2.splice(textIndex,1)
+        console.log(arrylist2)
+        changeUrl()
+    })
+    function changeUrl(){
+        $(".ba_change").attr('href','brand.html?brandList='+brandList+'&arrylist1='+arrylist1+'&arrylist2='+arrylist2+'')
+        $(".bs_noChose").attr('href','brand.html?brandList='+brandList+'&arrylist1='+arrylist1+'&arrylist2='+arrylist2+'')
+    }
 })
 
